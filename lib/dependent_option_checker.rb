94f9b15fe7e9d9fe42ceb137d874d2b264bce29c
muryoimpl <muryoimpl@gmail.com>
2025-03-01 21:26:31 +0900

    Run Creating gem 'dependent_option_checker'... github is already configured, ignoring --ci flag. MIT License enabled in config rubocop is already configured, ignoring --linter flag. RuboCop enabled in config Initializing git repo in /Users/muryoimpl/tmp/ruby/test-gem/dependent_option_checker/dependent_option_checker    identical  dependent_option_checker/Gemfile    identical  dependent_option_checker/lib/dependent_option_checker.rb    identical  dependent_option_checker/lib/dependent_option_checker/version.rb    identical  dependent_option_checker/sig/dependent_option_checker.rbs    identical  dependent_option_checker/dependent_option_checker.gemspec    identical  dependent_option_checker/Rakefile    identical  dependent_option_checker/README.md    identical  dependent_option_checker/bin/console    identical  dependent_option_checker/bin/setup    identical  dependent_option_checker/.gitignore    identical  dependent_option_checker/.rspec    identical  dependent_option_checker/spec/spec_helper.rb    identical  dependent_option_checker/spec/dependent_option_checker_spec.rb    identical  dependent_option_checker/.github/workflows/main.yml    identical  dependent_option_checker/LICENSE.txt    identical  dependent_option_checker/.rubocop.yml Gem 'dependent_option_checker' was successfully created. For more information on making a RubyGem visit https://bundler.io/guides/creating_gem.html

---
+++ lib/dependent_option_checker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dependent_option_checker/version"
+
+module DependentOptionChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
