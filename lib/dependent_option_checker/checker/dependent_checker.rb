b9825bfbe1489ab05009a64afad47a00b28c547d
muryoimpl <muryoimpl@gmail.com>
2025-03-27 10:08:55 +0900

    Move methods in dependent_checker.rb into checker.rb

--- lib/dependent_option_checker/checker/dependent_checker.rb
+++ lib/dependent_option_checker/checker/dependent_checker.rb
@@ -5,10 +5,6 @@ require 'active_record/reflection'
 module DependentOptionChecker
   class Checker
     class DependentChecker
-      TARGET_CLASSES = [
-        ActiveRecord::Reflection::HasOneReflection,
-        ActiveRecord::Reflection::HasManyReflection
-      ].freeze
       DEPENDENT_OPTION_VALUES = %i[
         destroy
         destroy_async
@@ -18,32 +14,7 @@ module DependentOptionChecker
         restrict_with_error
       ].freeze
 
-      attr_reader :model_class
-
-      def initialize(model_class, config)
-        super()
-        @model_class = model_class
-        @config = config
-        @valid = true
-      end
-
-      def execute
-        model_class.reflections.each_value do |reflection|
-          next unless TARGET_CLASSES.include?(reflection.class)
-          next if dependent_option_specified?(reflection.options)
-
-          @valid = false
-          # TODO: 設定漏れを出力する
-        end
-        # TODO: どのテーブルのどのリレーションが設定漏れかを出力する
-      end
-
-      def valid? = @valid
-      def invalid? = !@valid
-
-      private
-
-      def dependent_option_specified?(options)
+      def self.dependent_option_specified?(options)
         options.key?(:dependent) &&
           (
             DEPENDENT_OPTION_VALUES.include?(options[:dependent]) ||
