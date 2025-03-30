c74c55d8ef474e60454aaf1b0db03581deb901f6
muryoimpl <muryoimpl@gmail.com>
2025-03-30 16:12:07 +0900

    Refactor DependentOptionChecker::Checker::DependentChecker

--- lib/dependent_option_checker/checker/dependent_checker.rb
+++ lib/dependent_option_checker/checker/dependent_checker.rb
@@ -14,7 +14,25 @@ module DependentOptionChecker
         restrict_with_error
       ].freeze
 
-      def self.dependent_option_specified?(options)
+      def initialize(model)
+        @model = model
+      end
+
+      def extract_unspecified_relations
+        reflections.filter_map do |reflection|
+          option_specified?(reflection.options) ? nil : reflection.name.to_s
+        end
+      end
+
+      private
+
+      def reflections
+        @model.reflections.select do |_, reflection|
+          ::DependentOptionChecker::Checker::TARGET_CLASSES.include?(reflection.class)
+        end.values
+      end
+
+      def option_specified?(options)
         options.key?(:dependent) &&
           (
             DEPENDENT_OPTION_VALUES.include?(options[:dependent]) ||
--- spec/dependent_option_checker/checker/dependent_checker_spec.rb
+++ spec/dependent_option_checker/checker/dependent_checker_spec.rb
@@ -3,116 +3,39 @@
 require 'spec_helper'
 
 RSpec.describe DependentOptionChecker::Checker::DependentChecker do
-  describe '#execute' do
-    let(:klass) { Class.new }
+  describe '#extract_unspecified_relations' do
+    subject { described_class.new(model).extract_unspecified_relations }
 
-    subject do
-      checker.execute
-      checker
+    let(:model) { stub_const('Company', Class.new { |_| def self.pluralize_table_names = 'organizations' }) }
+
+    before do
+      allow(model).to receive(:reflections).and_return(
+        'organizations' => ActiveRecord::Reflection::HasManyReflection.new(:organizations, nil, options, model)
+      )
     end
 
-    context 'when specifying dependent option' do
-      let(:checker) { described_class.new(klass, {}) }
-      let(:dependent_value) { :destroy }
+    context 'when :dependent is not specified' do
+      let(:options) { {} }
 
-      before do
-        allow(klass).to receive(:reflections).and_return(
-          {
-            'organizations' => ActiveRecord::Reflection::HasManyReflection.new(
-              'organizations', nil, { dependent: dependent_value }, Organization
-            )
-          }
-        )
-      end
-
-      context 'when option is dependent: :destory' do
-        let(:dependent_value) { :destroy }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when option is dependent: :destroy_async' do
-        let(:dependent_value) { :destroy_async }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when option is dependent: :delete_all' do
-        let(:dependent_value) { :delete_all }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when option is dependent: :nullify' do
-        let(:dependent_value) { :nullify }
-        it { is_expected.to be_valid }
-      end
-      context 'when option is dependent: :restrict_with_exception' do
-        let(:dependent_value) { :restrict_with_exception }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when option is dependent: :restrict_with_error' do
-        let(:dependent_value) { :restrict_with_error }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when option is dependent: nil' do
-        let(:dependent_value) { nil }
-        it { is_expected.to be_valid }
-      end
-
-      context 'when option is unknown value' do
-        let(:dependent_value) { :unknown }
-        it { is_expected.to be_invalid }
-      end
+      it { is_expected.to match_array %w[organizations] }
     end
 
-    context 'when instance is not in target classes' do
-      let(:checker) { described_class.new(klass, {}) }
+    context 'when :dependent is specified and value is valid' do
+      let(:options) { { dependent: :destroy } }
 
-      before do
-        allow(klass).to receive(:reflections).and_return(
-          {
-            'organizations' => ActiveRecord::Reflection::ThroughReflection.new(
-              ActiveRecord::Reflection::HasManyReflection.new(
-                'members', nil, {}, Member
-              )
-            )
-          }
-        )
-      end
-
-      it { is_expected.to be_valid }
+      it { is_expected.to be_empty }
     end
 
-    context 'when not specifying dependent option on has_many' do
-      let(:checker) { described_class.new(klass, {}) }
+    context 'when :dependent is specified and value is invalid' do
+      let(:options) { { dependent: :invalid } }
 
-      before do
-        allow(klass).to receive(:reflections).and_return(
-          {
-            'organizations' => ActiveRecord::Reflection::HasManyReflection.new(
-              'organizations', nil, {}, Organization
-            )
-          }
-        )
-      end
-
-      it { is_expected.to be_invalid }
+      it { is_expected.to match_array %w[organizations] }
     end
 
-    context 'when not specifying dependent option on has_one' do
-      let(:checker) { described_class.new(klass, {}) }
+    context 'when :dependent is specified and value is nil' do
+      let(:options) { { dependent: nil } }
 
-      before do
-        allow(klass).to receive(:reflections).and_return(
-          {
-            'account' => ActiveRecord::Reflection::HasOneReflection.new(
-              'account', nil, {}, Account
-            )
-          }
-        )
-      end
-
-      it { is_expected.to be_invalid }
+      it { is_expected.to be_empty }
     end
   end
 end
